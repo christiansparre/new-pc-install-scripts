a15fc05e0759fbb2662a8050250fd58c4a84fe89
Christian Sparre <christian@sparre.it>
2024-02-12 18:36:19 +0100

    Create winget.ps1

---
+++ winget/winget.ps1
@@ -0,0 +1,15 @@
+winget install -h -e Microsoft.WindowsTerminal 
+# winget install -h -e Microsoft.VisualStudioCode -- does not add context menu's install manually
+winget install -h -e JetBrains.ReSharper
+winget install -h -e GitExtensionsTeam.GitExtensions
+winget install -h -e Git.Git
+winget install -h -e Microsoft.PowerShell
+winget install -h -e Microsoft.AzureCLI
+winget install -h -e 7zip.7zip
+winget install -h -e Microsoft.PowerToys
+winget install -h -e Microsoft.SQLServerManagementStudio
+winget install -h -e Microsoft.AzureStorageExplorer
+winget install -h -e KDE.KDiff3
+winget install -h -e GitHub.GitHubDesktop
+winget install -h -e Docker.DockerDesktop
+winget install -h -e LINQPad.LINQPad8
