61a5c1f85d7061c4f35258faddd8d3b9a2538f9f
Christian Sparre <christian@sparre.it>
2026-03-17 21:06:43 +0100

    Fix package ID for Azure Storage Explorer

--- install-winget-packages.ps1
+++ install-winget-packages.ps1
@@ -7,5 +7,5 @@ winget install -h -e --id GitHub.GitHubDesktop --accept-package-agreements --acc
 winget install -h -e --id Microsoft.AzureCLI --accept-package-agreements --accept-source-agreements
 winget install -h -e --id 7zip.7zip --accept-package-agreements --accept-source-agreements
 winget install -h -e --id Microsoft.PowerToys --accept-package-agreements --accept-source-agreements
-winget install -h -e --id Microsoft.AzureStorageExplorer --accept-package-agreements --accept-source-agreements
+winget install -h -e --id Microsoft.Azure.StorageExplorer --accept-package-agreements --accept-source-agreements
 winget install -h -e --id LINQPad.LINQPad.9 --accept-package-agreements --accept-source-agreements
